8025913221ce9b5849023c7f8439e5eb21935cb7
Sampo Juustila <sampo.juustila@iki.fi>
2022-12-11 21:05:21 +0200

    Import-skripti uudelle Nordea csv formaatille

--- jasenmaksu-import-nordea2.rb
+++ jasenmaksu-import-nordea2.rb
@@ -5,20 +5,18 @@
 # Lukee Nordean tuottaman tilitapahtuma-export-tiedoston ja päivittää
 # jäsenmaksutiedot jäsenrekisteriin.
 #
-# Tiedostonimi 'Tapahtumat_<tilinro>_<alkupvm>_<loppupvm>.txt',
-# tab-separated-formaatti
-# 
+# Tiedostonimi 'SHEKKITILI <tilinro> - <pvm>.csv',
+# semicolon-separated-formaatti
 #
 # Käyttö:
 #    ./jasenmaksu-import-nordea.rb <tilitapahtumat.csv>
 #
 
-require 'date'
-require_relative 'settings.rb'
-require_relative 'userdatabase.rb'
+require "date"
+require_relative "settings.rb"
+require_relative "userdatabase.rb"
 
-require_relative 'helpers.rb'
-
+require_relative "helpers.rb"
 
 if ARGV.length != 1
   puts "Usage:"
@@ -28,11 +26,9 @@ end
 
 INPUT = ARGV[0]
 
-
 $db = UserDatabase.new
 
-
-def process_payment(summa, viite, maksaja, pvm, viesti)
+def process_payment(summa, viite, maksaja, pvm)
   viite = viite.gsub(" ", "")
   if viite =~ /^(?<jasennro>[0-9]{5})(?<vuosi>20[123][0-9])[0-9]$/
     jasennro = $~[:jasennro]
@@ -48,48 +44,44 @@ def process_payment(summa, viite, maksaja, pvm, viesti)
         puts "Oli jo maksanut:  #{pvm}  #{summa}  #{maksaja}"
       end
     else
-      puts "Jäsentä ei löytynyt jäsennumerolla #{jasennro}:  #{pvm}  #{summa}  #{maksaja}  viite: #{viite}  viesti: #{viesti}"
+      puts "Jäsentä ei löytynyt jäsennumerolla #{jasennro}:  #{pvm}  #{summa}  #{maksaja}  viite: #{viite}"
     end
   else
-    puts "Tuntematon viitenumero:  #{pvm}  #{summa}  #{maksaja}  viite: #{viite}  viesti: #{viesti}"
+    puts "Tuntematon viitenumero:  #{pvm}  #{summa}  #{maksaja}  viite: #{viite}"
   end
   return false
 end
 
-
 count = 0
 File.open(INPUT, "r") do |file|
   file.each_line do |line|
-  
+
     # Ignore lines that don't start with a date
     if !(line =~ /^[0-9]/)
       next
     end
 
     # Parse the line
-    # 0:Kirjauspäivä 1:Arvopäivä 2:Maksupäivä 3:Määrä 4:Saaja/Maksaja 5:Tilinumero 6:BIC 7:Tapahtuma 8:Viite 9:Maksajan viite 10:Viesti 11:Kortinnumero 12:Kuitti	
-    split = line.split("\t", 15)
-    if split.length != 14
+    # Kirjauspäivä;Määrä;Maksaja(tilinro);Maksunsaaja(tilinro);Nimi;Otsikko;Viitenumero;Valuutta
+    split = line.split(";", 9)
+    if split.length != 8
       puts "Invalid line: #{line}"
       next
     end
-    pvm = split[1]
-    summa = split[3]
-    maksaja = split[4]
-    viite = split[8]
-    viesti = split[10]
+    pvm = split[0]
+    summa = split[1]
+    maksaja = split[5] # maksajan nimi on otsikko-kentässä
+    viite = split[6]
     if summa =~ /^-/
       # Payment, not receival
       next
     end
-    if process_payment(summa, viite, maksaja, pvm, viesti)
-      count+=1
+    if process_payment(summa, viite, maksaja, pvm)
+      count += 1
     end
-
   end
 end
 
 $db.save
 
 puts "Lisättiin #{count} maksanutta jäsentä."
-
